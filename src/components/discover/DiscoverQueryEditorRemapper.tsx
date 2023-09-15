923864bced3f83d5ca4dc256134c71f65f410885
GalvinGao <me@galvingao.com>
2023-09-15 17:16:10 -0400

    feat: remove discover

--- src/components/discover/DiscoverQueryEditorRemapper.tsx
+++ src/components/discover/DiscoverQueryEditorRemapper.tsx
@@ -3,6 +3,35 @@ import { FC } from "react"
 import { Control, useController } from "react-hook-form"
 import { QueryInput } from "../../components/discover/DiscoverQuerier"
 
+// Remapper shall be a string that:
+// - contains `content`
+// - has 1 or more pair of matching parentheses `()`
+const validateRemapper = (value: string) => {
+  const contentIndex = value.indexOf("content")
+  if (contentIndex === -1) {
+    return "重映射规则必须包含向 content 的引用"
+  }
+
+  let currentDepth = 0
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] === "(") {
+      currentDepth++
+    } else if (value[i] === ")") {
+      currentDepth--
+    }
+
+    if (currentDepth < 0) {
+      return "重映射规则中的括号不匹配：括号使用必须严格嵌套，但是出现了多余的右括号"
+    }
+  }
+
+  if (currentDepth > 0) {
+    return "重映射规则中的括号不匹配：括号使用必须严格嵌套，但是出现了多余的左括号"
+  }
+
+  return true
+}
+
 export const DiscoverQueryEditorRemapper: FC<{
   control: Control<QueryInput>
 }> = ({ control }) => {
@@ -12,7 +41,7 @@ export const DiscoverQueryEditorRemapper: FC<{
   } = useController({
     name: "resultMappingInput",
     control,
-    rules: { required: "请填写重映射规则" },
+    rules: { required: "请填写重映射规则", validate: validateRemapper },
   })
 
   return (
@@ -22,6 +51,7 @@ export const DiscoverQueryEditorRemapper: FC<{
         label="重映射规则"
         error={!!error}
         helperText={error?.message}
+        className="font-mono"
       />
     </FormControl>
   )
